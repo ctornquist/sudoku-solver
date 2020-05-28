5c64f3570e9e2b7e03c02186f01f4df1eb585502
Chase J. Krivickas 21 <ckrivickas@flume.cs.dartmouth.edu>
2020-05-28 10:47:19 -0400

    put solver functions into the common dir

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ PROG = sudoku
 OBJS = 
 LLIBS = ../libcs50/libcs50-given.a ../common/common.a
 LIBS = -lm
+MAKE = make
 
 #RULES
 $(PROG): $(OBJS) $(LLIBS)
@@ -21,3 +22,12 @@ clean:
 	rm -f *~ *.o *.dSYM
 	rm -f $(PROG)
 	rm -f core
+
+############## clean  ##########
+all clean:
+	rm -f *~
+	rm -f TAGS
+	$(MAKE) -C libcs50 clean
+	$(MAKE) -C common clean
+	$(MAKE) -C solver clean
+	# $(MAKE) -C creator clean
---
+++ common/functions.h
@@ -0,0 +1,39 @@
+/* 
+ * functions.h
+ * 
+ * Functions used by the solver and creator 
+ *
+ * CS50 - May 2020
+ * Tornquist, Palmer, Wagner and Krivickas
+ *
+ */
+
+#define _GNU_SOURCE 
+#include<stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+//*************** getBoard ***************//
+/* Loads the puzzle into an array from stdin and returns the array when complete. Assumes that 
+ * blank spaces in the sudoku puzzle are filled in with zeros. Array is indexed from 0-80 with
+ * the first row 0-8, second row 9-17 and so on.
+ */
+int *getBoard();
+
+//*************** solve ***************//
+/* User provides an array representing the sudoku board, function will update list to include the 
+ * numbers it solves for, returning true if it's solveable. 
+ */
+bool solve(int *list);
+
+//*************** isValid ***************//
+/* Checks if the number given is valid, ie does not exist in the row, column, or box. 
+ * 
+ * User provides the number to check, the index in which it occurs in the array (sudoku puzzle) 
+ * and the array that represents the puzzle itself. 
+ */
+bool isValid(int number, int index, int *list);
